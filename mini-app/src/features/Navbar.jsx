98e645207514f87ddd57aadd4c4a886c12355dcb
darsshu <chhatroladarshan9@gmail.com>
2024-09-26 15:17:30 +0530

    homepage

--- mini-app/src/features/Navbar.jsx
+++ mini-app/src/features/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import darshan from '../assets/darshan.jpg'
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+
 
 
 const Navbar = () => {
+
     const handleClick = () => {
 
-        const pdfUrl = '../assets/Darshanchhatrola.CV.docx'
+        const pdfUrl = '/src/assets/Darshan_Resume.pdf'
         const fileName = 'Darshanchhatrola.docx'
 
         const link = document.createElement('a')
         link.href = pdfUrl
-        link.download = fileName
+        link.download = true
 
         document.body.appendChild(link)
 
@@ -28,32 +31,35 @@ const Navbar = () => {
             <div className='bg-slate-800'>
 
 
-                <div className=' bg-black ' id='header'>
+                <div className=' bg-black bg-opacity-30 fixed w-full' id='header'>
                     <div className='flex justify-around'>
                         <div className='content-center'>
+
                             <Link to={''} className='text-orange-400 text-4xl font-serif'>
                                 {"<Darshan Chhatrola/>"}
                             </Link>
 
                         </div>
                         <div className='flex '>
+
                             <div className=''>
-                                <Link to={''} className="block px-4 py-2 hover:underline font-semibold hover:text-yellow-300 mt-2 mb-2   text-blue-500 text-xl data-[focus]:bg-gray-100">
+
+                                <Link to={''} className="block px-4 py-2 hover:underline font-semibold hover:text-yellow-300 mt-2 mb-2  transition-all duration-1000 text-blue-500 text-xl data-[focus]:bg-gray-100">
                                     Home
                                 </Link>
                             </div>
                             <div className=' '>
-                                <Link to={'/aboutus'} className="block px-4 py-2 hover:underline font-semibold hover:text-yellow-300 mt-2 mb-2 text-blue-500 text-xl data-[focus]:bg-gray-100">
+                                <Link to={'/aboutus'} className="block px-4 py-2 hover:underline font-semibold hover:text-yellow-300 mt-2 mb-2transition-all duration-1000 text-blue-500 text-xl data-[focus]:bg-gray-100">
                                     About Us
                                 </Link>
                             </div>
                             <div className=' '>
-                                <Link to={'/contactus'} className="block px-4 py-2 hover:underline font-semibold hover:text-yellow-300 mt-2 mb-2   text-blue-500 text-xl data-[focus]:bg-gray-100">
+                                <Link to={'/contactus'} className="block px-4 py-2 hover:underline font-semibold hover:text-yellow-300 mt-2 mb-2 transition-all duration-1000  text-blue-500 text-xl data-[focus]:bg-gray-100">
                                     Contach Us
                                 </Link>
                             </div>
                             <div className=' '>
-                                <Link to={''} className="block px-4 py-2 hover:underline font-semibold hover:text-yellow-300 mt-2 mb-2   text-blue-500 text-xl data-[focus]:bg-gray-100">
+                                <Link to={''} className="block px-4 py-2 hover:underline font-semibold hover:text-yellow-300 mt-2 mb-2  transition-all duration-1000 text-blue-500 text-xl data-[focus]:bg-gray-100">
                                     Project
                                 </Link>
                             </div>
@@ -73,12 +79,41 @@ const Navbar = () => {
 
                 </div>
 
-                <div className='' id='home'>
+                <div className='flex justify-center min-h-screen ' id='home'>
+                    <div className='text-center mt-28' id='home-contant'>
+                        <p className='text-white block px-6 py-2   mt-2 mb-2 font-thin'>Hello I am</p>
+                        <h1 className='text-white block px-6 py-2   mt-2 mb-2 font-bold text-4xl'>Darshan Chhatrola</h1>
+                        <p className='font-bold block px-6 py-2   mt-2 mb-2 text-2xl text-green-500'>React-JS Devloper</p>
+
+                        <button onClick={handleClick} className="font-bold border-solid rounded-xl transition-all duration-1000 hover:text-yellow-500 bg-gray-700 hover:bg-black  h-12 w-52 hover:scale-125 mt-8 mb-2 text-2xl text-white ">
+                            Contact
+                        </button>
+                        <div className=' flex space-x-8 ml-24 mt-5 size-1/2 ' id='contact-icon'>
+                            <Link target='_blank' to={'https://wa.me/8200505891'} className='' id=''>
+                                <FaWhatsapp className='text-4xl text-green-500 hover:scale-150' />
+                            </Link>
+                            <Link target='_blank' to={'https://github.com/darsshu'} className='' id=''>
+                                <FaGithub className='text-4xl text-white hover:scale-150' />
+                            </Link>
+                            <Link target='_blank' to={'https://www.linkedin.com/in/darshan-chhatrola-046299311/'} className='' id=''>
+                                <FaLinkedin className='text-4xl text-blue-700 hover:scale-150' />
+                            </Link>
+
+
+
+                        </div>
+
+
+
+                    </div>
+                    <div className='' id='home-image'>
+
+                    </div>
 
                 </div>
 
                 <div className='' id='aboutus'>
-                    <div className=' flex  text-center  mt-24 ' >
+                    <div className=' flex  text-center  ' >
                         <div className=' ms-28 '>
                             <img className='  rounded-md w-2/3  ' src={darshan} />
 
